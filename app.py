bcff87ba1227dbb416d23089ce713e5c38954966
namnnb <namnnb@fpt.com.com>
2021-07-16 21:35:23 +0700

    edit file app

--- app.py
+++ app.py
@@ -19,19 +19,14 @@ from gevent.pywsgi import WSGIServer
 # Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
+# gắn đường dẫn của model vô đây
 MODEL_PATH = 'models/model_resnet.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
 model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
+
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
